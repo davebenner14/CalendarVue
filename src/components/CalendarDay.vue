090dc972849638543de08836d60df74a0734c7ff
David Benner <davidbenner@davids-mbp.lan>
2023-07-13 10:10:24 -0400

    Create CalendarDay component

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="day column" @click="setActiveDay(day.id)">
+    <div class="day-banner has-text-white has-text-centered">
+      {{ day.abbvTitle }}
+    </div>
+    <div class="day-details">
+      <div class="day-number">{{ day.id }}</div>
+      <CalendarEvent
+        v-for="event in day.events"
+        :key="day.events.indexOf(event)"
+        :event="event"
+        :day="day"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store.js";
+import CalendarEvent from "./CalendarEvent.vue";
+
+export default {
+  name: "CalendarDay",
+  components: {
+    CalendarEvent,
+  },
+  props: ["day"],
+  methods: {
+    setActiveDay(dayId) {
+      store.setActiveDay(dayId);
+    },
+  },
+};
+</script>
