f2978bb42771299f9e5506e44e6fe30ac5744994
David Benner <davidbenner@davids-mbp.lan>
2023-07-13 10:11:24 -0400

    Create CalendarEvent component

---
+++ src/components/CalendarEvent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="day-event" :style="getEventBackgroundColor">
+    <div v-if="!event.edit">
+      <span class="has-text-centered details">{{ event.details }}</span>
+      <div class="has-text-centered icons">
+        <i
+          class="fa fa-pencil-square edit-icon"
+          @click="editEvent(day.id, event.details)"
+        ></i>
+        <i
+          class="fa fa-trash-o delete-icon"
+          @click="deleteEvent(day.id, event.details)"
+        ></i>
+      </div>
+    </div>
+    <div v-if="event.edit">
+      <input
+        v-model="newEventDetails"
+        type="text"
+        :placeholder="event.details"
+      />
+      <div class="has-text-centered icons">
+        <i
+          class="fa fa-check"
+          @click="updateEvent(day.id, event.details, newEventDetails)"
+        ></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store.js";
+
+export default {
+  name: "CalendarEvent",
+  props: ["day", "event"],
+  data() {
+    return {
+      newEventDetails: "",
+    };
+  },
+  methods: {
+    editEvent(dayId, eventDetails) {
+      store.editEvent(dayId, eventDetails);
+    },
+    updateEvent(dayId, originalEventDetails, updatedEventDetails) {
+      if (updatedEventDetails === "")
+        originalEventDetails = updatedEventDetails;
+      store.updateEvent(dayId, originalEventDetails, updatedEventDetails);
+      this.newEventDetails = "";
+    },
+    deleteEvent(dayId, eventDetails) {
+      store.deleteEvent(dayId, eventDetails);
+    },
+  },
+  computed: {
+    getEventBackgroundColor() {
+      const colors = ["#FF9999", "#85D6FF", "#99FF99"];
+      let randomColor = colors[Math.floor(Math.random() * colors.length)];
+      return `background-color: ${randomColor}`;
+    },
+  },
+};
+</script>
