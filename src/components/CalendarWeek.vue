ac09128510d3d0473ba2caa1fa5c3c2bb8c14ffd
David Benner <davidbenner@davids-mbp.lan>
2023-07-13 10:11:55 -0400

    Create CalendarWeek component

---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-mobile">
+      <CalendarDay
+        v-for="day in sharedState.seedData"
+        :key="day.id"
+        :day="day"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store.js";
+import CalendarDay from "./CalendarDay.vue";
+
+export default {
+  name: "CalendarWeek",
+  components: {
+    CalendarDay,
+  },
+  data() {
+    return {
+      sharedState: store.state, // access state object
+    };
+  },
+};
+</script>
