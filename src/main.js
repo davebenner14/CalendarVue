86812099e3049ce95662c164232b149ccb9babfa
David Benner <davidbenner@davids-mbp.lan>
2023-07-13 09:49:15 -0400

    Install and import Bulma CSS and FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
