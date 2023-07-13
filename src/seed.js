7eb19ff2b6dd245f2232ddc1a56389f4c2b349f2
David Benner <davidbenner@davids-mbp.lan>
2023-07-13 10:01:56 -0400

    Complete seed data for each day of the week

---
+++ src/seed.js
@@ -0,0 +1,72 @@
+export const seedData = [
+  {
+    id: 1,
+    abbvTitle: "Mon",
+    fullTitle: "Monday",
+    events: [
+      { details: "Get Groceries", edit: false },
+      { details: "Carpool", edit: false },
+    ],
+    active: true,
+  },
+  {
+    id: 2,
+    abbvTitle: "Tue",
+    fullTitle: "Tuesday",
+    events: [
+      { details: "Meeting with Boss", edit: false },
+      { details: "Gym", edit: false },
+    ],
+    active: false,
+  },
+  {
+    id: 3,
+    abbvTitle: "Wed",
+    fullTitle: "Wednesday",
+    events: [
+      { details: "Take Dog to Vet", edit: false },
+      { details: "Swimming Lessons", edit: false },
+    ],
+    active: false,
+  },
+  {
+    id: 4,
+    abbvTitle: "Thu",
+    fullTitle: "Thursday",
+    events: [
+      { details: "Finish Report", edit: false },
+      { details: "Book Club", edit: false },
+    ],
+    active: false,
+  },
+  {
+    id: 5,
+    abbvTitle: "Fri",
+    fullTitle: "Friday",
+    events: [
+      { details: "Happy Hour", edit: false },
+      { details: "Dinner with Friends", edit: false },
+    ],
+    active: false,
+  },
+  {
+    id: 6,
+    abbvTitle: "Sat",
+    fullTitle: "Saturday",
+    events: [
+      { details: "Hiking Trip", edit: false },
+      { details: "Grocery Shopping", edit: false },
+    ],
+    active: false,
+  },
+  {
+    id: 7,
+    abbvTitle: "Sun",
+    fullTitle: "Sunday",
+    events: [
+      { details: "Relaxing", edit: false },
+      { details: "Preparing for the Week", edit: false },
+    ],
+    active: false,
+  },
+];
